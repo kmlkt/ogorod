c9a18090b7c1c1c8fb51dbb828dbd09bad4f46a4
kogo11 <>
2024-09-14 20:18:04 +0500

    Чтение, запись в yaml. Клонирование с Github

--- main.go
+++ main.go
@@ -4,63 +4,70 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"os/exec"
+
+	"github.com/go-git/go-git/v5"
+	"gopkg.in/yaml.v3"
 )
 
 type Site struct {
 	Repository string
 	LocalPath  string
 	Domain     string
-	Url        string
+	URL        string
 }
 
-var sitesEnabled = "/etc/nginx/sites-enabled/ogorod"
+type SitesConfig struct {
+	Sites []Site
+}
 
-func CreateNginxConfig(sites []Site) {
-	servers := map[string][]Site{}
-	for _, site := range sites {
-		servers[site.Domain] = append(servers[site.Domain], site)
-	}
-	file, err := os.OpenFile(sitesEnabled, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0755)
+func gitclone(url string) {
+	destination := "./repository"
+	_, err := git.PlainClone(destination, false, &git.CloneOptions{
+		URL: url,
+	})
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error cloning repository: %v", err)
 	}
-	defer file.Close()
-	for serverName, serverSites := range servers {
-		fmt.Fprintf(file, `
-server {
-	listen 80;
-	server_name %s;
-	index index.html;
-		`, serverName)
-		for _, site := range serverSites {
-			fmt.Fprintf(file, `
-	location %s {
-		alias %s;
-		try_files $uri $uri/ =404;
-	}
-`, site.Url, site.LocalPath)
-		}
-		fmt.Fprint(file, "}")
+	fmt.Println("Repository cloned successfully!")
+}
+
+func clonesitefromsettings(config SitesConfig) {
+	for _, site := range config.Sites {
+		gitclone(site.URL)
 	}
 }
 
-func RestartNginx() {
-	cmd := exec.Command("systemctl", "restart", "nginx")
-	fmt.Print(cmd)
-	if cmd.Err != nil {
-		log.Fatal(cmd.Err)
-	}
-	err := cmd.Run()
+func addnewsites(config SitesConfig) {
+	var repository string
+	var localpath string
+	var domain string
+	var url string
+	fmt.Print("Введите репозиторий: ")
+	fmt.Scan(&repository)
+	fmt.Print("Введите путь хранения файлов: ")
+	fmt.Scan(&localpath)
+	fmt.Print("Введите домен: ")
+	fmt.Scan(&domain)
+	fmt.Print("Введите URL: ")
+	fmt.Scan(&url)
+	config.Sites = append(config.Sites, Site{repository, localpath, domain, url})
+	updatedYAML, err := yaml.Marshal(config)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Ошибка при кодировании YAML: %v", err)
+	}
+	err = os.WriteFile("settings.yaml", updatedYAML, 0666)
+	if err != nil {
+		log.Fatalf("Ошибка при записи файла: %v", err)
 	}
 }
 
 func main() {
-	CreateNginxConfig([]Site{
-		{Domain: "example.org", Url: "/", LocalPath: "/dist/"},
-		{Domain: "www.example.org", Url: "/naher/", LocalPath: "/dist/"},
-	})
-	RestartNginx()
+	yamlFile, err := os.ReadFile("./settings.yaml")
+	if err != nil {
+		log.Fatalf("Ошибка при чтении файла: %v", err)
+	}
+	var config SitesConfig
+	if err := yaml.Unmarshal(yamlFile, &config); err != nil {
+		log.Fatalf("Ошибка при декодировании YAML: %v", err)
+	}
 }
