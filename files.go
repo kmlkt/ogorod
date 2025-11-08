e56335d985fd75a7fb7c84880dbe54020310c1ff
karim <kmlkt@bk.ru>
2025-11-08 23:43:53 +0500

    auto commit

--- files.go
+++ files.go
@@ -3,10 +3,15 @@ package ogorod
 import (
 	"io"
 	"os"
+	"os/exec"
 	"path/filepath"
 )
 
-var basePath = "./"
+var basePath string
+
+func init() {
+	basePath, _ = os.Getwd()
+}
 
 func OpenFile(path ...string) (*os.File, error) {
 	fullPath := filepath.Join(path...)
@@ -14,6 +19,31 @@ func OpenFile(path ...string) (*os.File, error) {
 	return os.OpenFile(fullPath, os.O_RDWR|os.O_CREATE, 0777)
 }
 
+func FileExists(path ...string) (bool, error) {
+	fullPath := filepath.Join(path...)
+	os.MkdirAll(filepath.Dir(fullPath), 0777)
+	_, err := os.Stat(fullPath)
+	if os.IsNotExist(err) {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	} else {
+		return true, nil
+	}
+}
+
+func ServiceFilePath(s Service, path []string) string {
+	return filepath.Join(append([]string{basePath, "services", s.Key()}, path...)...)
+}
+
 func OpenServiceFile(s Service, path ...string) (io.ReadWriter, error) {
-	return OpenFile(basePath, "services", s.Key(), filepath.Join(path...))
+	return OpenFile(ServiceFilePath(s, path))
+}
+
+func ServiceFileExists(s Service, path ...string) (bool, error) {
+	return FileExists(ServiceFilePath(s, path))
+}
+
+func RunServiceFile(s Service, path ...string) *exec.Cmd {
+	return exec.Command(ServiceFilePath(s, path))
 }
--- postgres.go
+++ postgres.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/kmlkt/ogorod/archive"
 )
@@ -19,14 +22,49 @@ func (p Postgres) Key() string {
 }
 
 func (p Postgres) Install() error {
+	if p.State() != Missing {
+		return nil
+	}
 	path := fmt.Sprintf(postgresPath, p.Version)
 	resp, err := http.Get(path)
 	if err != nil {
 		return err
 	}
 	err = archive.DecompressTarGz(resp.Body, func(path string) (io.Writer, error) {
-		return OpenServiceFile(p, "src", path)
+		firstDirIndex := strings.IndexRune(path, os.PathSeparator)
+		if firstDirIndex == -1 {
+			return nil, nil
+		}
+		pathWithoutFirstDir := path[firstDirIndex+1:]
+		return OpenServiceFile(p, "src", pathWithoutFirstDir)
 	})
+	if err != nil {
+		return err
+	}
+	configure := RunServiceFile(p, "src", "configure")
+	configure.Args = append(configure.Args, "--prefix="+ServiceFilePath(p, []string{}))
+	configure.Stdout = os.Stdout
+	configure.Stderr = os.Stdout
+	configure.Dir = ServiceFilePath(p, []string{"src"})
+	err = configure.Start()
+	if err != nil {
+		return err
+	}
+	err = configure.Wait()
+	if err != nil {
+		return err
+	}
+
+	makeInstall := exec.Command("make", "install")
+	makeInstall.Stdout = os.Stdout
+	makeInstall.Stderr = os.Stdout
+	makeInstall.Dir = ServiceFilePath(p, []string{"src"})
+	makeInstall.Env = configure.Env
+	err = makeInstall.Start()
+	if err != nil {
+		return err
+	}
+	err = makeInstall.Wait()
 	if err != nil {
 		return err
 	}
@@ -35,7 +73,12 @@ func (p Postgres) Install() error {
 }
 
 func (p Postgres) State() ServiceState {
-	return Idle
+	exists, err := ServiceFileExists(p, "postgres")
+	if !exists || err != nil {
+		return Missing
+	} else {
+		return Idle
+	}
 }
 
 func (p Postgres) Start() error {
